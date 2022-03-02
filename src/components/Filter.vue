b0ce1ed81c06ad04622d3edfc831fb27b88ed151
wiliam <wiliam@webcgi.com.br>
2022-03-02 18:14:39 -0300

    .

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -114,7 +114,7 @@
                 <v-divider class="mt-2"></v-divider> </template
             ></v-autocomplete>
             <span
-              v-show="!controller.stationsSelected.length > 0"
+              v-show="!controller.stationsTypesSelected.length > 0"
               style="color: red"
               >*selecione pelo menos 1 tipo de estação para desbloquear o
               seletor de estações</span
